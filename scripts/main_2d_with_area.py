3e2ac936074966c763a395cfe32c0bd0430ea8e3
lulu110997 <louis97fernandez@gmail.com>
2024-03-22 12:06:54 +1100

    added case with area

--- scripts/main_2d_with_area.py
+++ scripts/main_2d_with_area.py
@@ -17,15 +17,16 @@ SAVE_ANIMATION = False  # If you want to save an animation
 F_NAME = "static"  # Name of the gif animation
 SKIP_STEPS = 3  # Useful for skipping steps during simulation to increase speed
 
-# STATIC = False; START = 230; END = 500
-STATIC = True; START = 0; END = 500; STATIC_OBS_POS = (0.3, 0.1)  # 115 start and (0.3, 0.1) obs pos is default
+STATIC = False; START = 100; END = 500
+# STATIC = True; START = 0; END = 500; STATIC_OBS_POS = (0.4, 0.2)
 
 # CBF param
-CBF_GAMMA = 4  # Represents how close the agent can come to the obstacle (higher = closer)
+CBF_GAMMA = 1.4  # Represents how close the agent can come to the obstacle (higher = closer)
+# CBF_GAMMA = 0.1  # Represents how close the agent can come to the obstacle (higher = closer)
 
 # APF params
-APF_Q = 0.2  # Minimum allowable distance before the potential field is activated
-NETA = 0.5  # Gain for the repulsive field
+APF_Q = 0.1  # Minimum allowable distance before the potential field is activated
+NETA = 0.2  # Gain for the repulsive field
 
 # P controller used in cbf and apf
 KV = 1  # Velocity gain used when calculating velocity
@@ -33,6 +34,10 @@ MAX_VEL = 1.5  # Maximum allowable velocity
 MIN_VEL = -1.5  # Minimum allowable velocity
 
 TIME = 1  # Desired time to complete the task?
+
+# Radius of ee and obstacle
+R_OBS = 0.1
+R_EE = 0.1
 
 ########################################################################################################################
 if not SAVE_ANIMATION:
@@ -52,19 +57,19 @@ def distance_derivative():
     x_obs = sym.symbols('x_obs:2')
     x_ee = sym.Matrix([[x_ee[0], x_ee[1]]])
     x_obs = sym.Matrix([[x_obs[0], x_obs[1]]])
-    Rw = sym.Symbol('Rw')
-    R = sym.Symbol('R')
+    R_ee = sym.Symbol('R_ee')
+    R_obs = sym.Symbol('R_obs')
 
     # This provides a much more complicated hx_dot
     alt_hx = sym.sqrt(
         (x_ee[0] - x_obs[0])**2 +
         (x_ee[1] - x_obs[1])**2
-    ) - (R + Rw)
+    ) - (R_obs + R_ee)
 
     hx_dot = sym.diff(alt_hx, x_ee)
 
-    hx = sym.lambdify([x_ee, x_obs, R, Rw], expr=alt_hx)
-    hx_dot = sym.lambdify([x_ee, x_obs, R, Rw], expr=hx_dot)
+    hx = sym.lambdify([x_ee, x_obs, R_ee, R_obs], expr=alt_hx)
+    hx_dot = sym.lambdify([x_ee, x_obs, R_ee, R_obs], expr=hx_dot)
     return hx, hx_dot
 
 # Obtain the distance function and its derivative
@@ -102,9 +107,9 @@ def get_force(r, o):
     Returns: tuple | the robot's next position and calculated velocity
     """
     assert r.ndim == 1 and o.ndim == 1
-    curr_dist = dist_f(r, o, 0, 0)
+    curr_dist = dist_f(r, o, R_EE, R_OBS)
     if curr_dist < APF_Q:
-        nabla_d = dist_deriv(r, o, 0, 0).flatten()
+        nabla_d = dist_deriv(r, o, R_EE, R_OBS).flatten()
         force = -NETA * (1/APF_Q - 1/curr_dist) * (1/curr_dist)**2 * nabla_d
     else:
         force = np.array([0, 0])
@@ -124,10 +129,10 @@ def use_cbf(r, desired_pos, o):
     """
     R = np.identity(2)  # Weights?
     s = KV*((desired_pos - r) / DELTA_TIME)  # What we are trying to optimise
-    G = -dist_deriv(r, o, 0, 0)  # CBF derivative
-    h = CBF_GAMMA * dist_f(r, o, 0, 0)  # CBF exponential gamma*hx
+    G = -dist_deriv(r, o, R_EE, R_OBS)  # CBF derivative
+    h = np.array([CBF_GAMMA * dist_f(r, o, R_EE, R_OBS)])  # CBF exponential gamma*hx
 
-    vel = solve_ls(R, s, G, h, lb=np.array([MIN_VEL, MIN_VEL]), ub=np.array([MAX_VEL, MAX_VEL]), solver="osqp")  # osqp or cvxopt
+    vel = solve_ls(R, s, G, h, lb=np.array([MIN_VEL, MIN_VEL]), ub=np.array([MAX_VEL, MAX_VEL]), solver="osqp")  # osqp or clarabel or cvxopt
     return vel
 
 
@@ -146,9 +151,46 @@ def cap_vel(vel):
         vel[1] = math.copysign(MAX_VEL, vel[1])
     return vel
 
+def update(i):
+    # Plots trajectories
+    ee_circle = plt.Circle((robot_traj[i, 0], robot_traj[i, 1]), R_EE, fill=False, color='blue', label='desired')
+    apf_circle = plt.Circle((robot_pos_apf_history[i, 0], robot_pos_apf_history[i, 1]), R_EE, fill=False,
+                            color='cyan', label='apf')
+    cbf_circle = plt.Circle((robot_pos_cbf_history[i, 0], robot_pos_cbf_history[i, 1]), R_EE, fill=False,
+                            color='black', label='cbf')
+    ax[0].add_patch(ee_circle)
+    ax[0].add_patch(apf_circle)
+    ax[0].add_patch(cbf_circle)
+    if not STATIC:
+        obs_circle = plt.Circle((obstacle_traj[i, 0], obstacle_traj[i, 1]), R_OBS, fill=False, color='red')
+        ax[0].add_patch(obs_circle)
+
+    # Plots APF forces
+    ax[1].scatter(i, apf_force_history[i, 0], marker='_', color='red')
+    ax[1].scatter(i, apf_force_history[i, 1], marker='_', color='blue')
+
+    if not SAVE_ANIMATION and START < i < END:
+        # drawing updated values
+        fig.canvas.draw()
+        # This will run the GUI event loop until all UI events currently waiting have been processed
+        fig.canvas.flush_events()
+
+    if i < N_STEPS-1-SKIP_STEPS:
+        ee_circle.remove()
+        apf_circle.remove()
+        cbf_circle.remove()
+
+        if not STATIC:
+            obs_circle.remove()
+
+    return ax
+
 # Create an xy trajectory
 robot_traj = traj.ctraj(SE3(0, 0, 0), SE3(1, 0, 0), N_STEPS).t
-obstacle_traj = traj.ctraj(SE3(0.5, 0.5, 0), SE3(0.5, -0.5, 0), N_STEPS).t
+if STATIC:
+    obstacle_traj = STATIC_OBS_POS
+else:
+    obstacle_traj = traj.ctraj(SE3(0.5, 1, 0), SE3(0.5, -0.5, 0), N_STEPS).t
 
 # Obtain initial robot position for APF and CBF
 curr_robot_pos_apf = robot_traj[0, :2]
@@ -165,9 +207,9 @@ robot_vel_cbf_history = np.zeros((N_STEPS, 2))
 for i in range(1, N_STEPS):
     # P controller? incorporates delta time
     if STATIC:
-        curr_obstacle_pos = np.array(STATIC_OBS_POS); next_obstacle_pos = np.array(STATIC_OBS_POS)
+        curr_obstacle_pos = np.array(STATIC_OBS_POS)
     else:
-        curr_obstacle_pos = obstacle_traj[i-1, :2]; next_obstacle_pos = obstacle_traj[i, :2]
+        curr_obstacle_pos = obstacle_traj[i-1, :2]
     curr_robot_pos = robot_traj[i-1, :2]; next_robot_pos = robot_traj[i, :2]
     robot_vel = cap_vel(get_vel(curr_robot_pos, next_robot_pos))
 
@@ -213,34 +255,11 @@ pos_ax[1][1].plot(range(0, N_STEPS), np.round(robot_traj[:, 1] - robot_vel_cbf_h
 
 # Initialise plot
 fig.suptitle('Position over time and repulsive forces')
-ax[0].scatter(robot_traj[0, 0], robot_traj[0, 1], marker='.', color='blue', label='desired')
-ax[0].scatter(robot_pos_apf_history[0, 0], robot_pos_apf_history[0, 1], marker='.', color='cyan', label='apf')
-ax[0].scatter(robot_pos_cbf_history[0, 0], robot_pos_cbf_history[0, 1], marker='.', color='black', label='cbf')
-ax[1].scatter(0, apf_force_history[0, 0], marker='_', color='red', label="x repulsion")
-ax[1].scatter(0, apf_force_history[0, 1], marker='_', color='blue', label="y repulsion")
-
-
-def update(i):
-    # Plots trajectories
-    ax[0].scatter(robot_traj[i, 0], robot_traj[i, 1], marker='.', color='blue')
-    ax[0].scatter(robot_pos_apf_history[i, 0], robot_pos_apf_history[i, 1], marker='.', color='cyan')
-    ax[0].scatter(robot_pos_cbf_history[i, 0], robot_pos_cbf_history[i, 1], marker='.', color='black')
-    if STATIC:
-        ax[0].scatter(STATIC_OBS_POS[0], STATIC_OBS_POS[1], marker='*', color='red')
-    else:
-        ax[0].scatter(obstacle_traj[i, 0], obstacle_traj[i, 1], marker='*', color='red')
-
-    # Plots APF forces
-    ax[1].scatter(i, apf_force_history[i, 0], marker='_', color='red')
-    ax[1].scatter(i, apf_force_history[i, 1], marker='_', color='blue')
-
-    if not SAVE_ANIMATION and START < i < END :
-        # drawing updated values
-        fig.canvas.draw()
-        # This will run the GUI event loop until all UI events currently waiting have been processed
-        fig.canvas.flush_events()
-    return ax
-
+if STATIC:
+    obs_circle = plt.Circle((obstacle_traj[0], obstacle_traj[1]), R_OBS, fill=False, color='red')
+    ax[0].add_patch(obs_circle)
+ax[0].set_xlim([-0.25, 1.5])
+ax[0].set_ylim([-0.75, 0.75])
 
 if not SAVE_ANIMATION:
     for i in range(1, N_STEPS - 1, SKIP_STEPS):
